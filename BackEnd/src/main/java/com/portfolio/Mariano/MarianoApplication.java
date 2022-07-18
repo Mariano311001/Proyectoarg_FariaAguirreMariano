146966fbb80ff90bfdd2148754d541a1309d473f
Mariano Aguirre <104844333+Mariano311001@users.noreply.github.com>
2022-07-18 00:57:40 -0300

    Cambios

---
+++ BackEnd/src/main/java/com/portfolio/Mariano/MarianoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Mariano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarianoApplication.class, args);
+	}
+
+}
