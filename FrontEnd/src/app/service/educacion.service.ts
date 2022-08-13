dbf7bed8593012e5dd15df31ac7c3c33463eb2df
Mariano Aguirre <104844333+Mariano311001@users.noreply.github.com>
2022-08-13 00:55:42 -0300

    Cambios

--- FrontEnd/src/app/service/educacion.service.ts
+++ FrontEnd/src/app/service/educacion.service.ts
@@ -6,7 +6,7 @@ import { Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'https://localhost:8080/';
+  URL = 'http://localhost:8080/educacion/';
 
   constructor(private httpClient : HttpClient) { }
 
