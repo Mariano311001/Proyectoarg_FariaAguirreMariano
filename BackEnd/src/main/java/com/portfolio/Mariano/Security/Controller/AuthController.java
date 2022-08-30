dbc4e29858eb066bd5d5deab9ea96f506334e7f9
Mariano Aguirre <104844333+Mariano311001@users.noreply.github.com>
2022-08-30 17:52:09 -0300

    Cambios

--- BackEnd/src/main/java/com/portfolio/Mariano/Security/Controller/AuthController.java
+++ BackEnd/src/main/java/com/portfolio/Mariano/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin("https://frontendmariano.web.app")
+@CrossOrigin("https://backendmariano.herokuapp.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
