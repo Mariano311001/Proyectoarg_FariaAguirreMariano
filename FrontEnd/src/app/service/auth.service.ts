f9c0de21e9302e97708dce0dff4af77e54ecaea5
Mariano Aguirre <104844333+Mariano311001@users.noreply.github.com>
2022-08-31 18:30:14 -0300

    Problema de cors

--- FrontEnd/src/app/service/auth.service.ts
+++ FrontEnd/src/app/service/auth.service.ts
@@ -9,15 +9,15 @@ import { JwtDto } from '../model/jwt-dto';
   providedIn: 'root'
 })
 export class AuthService {
- authURL = 'https://backendmariano.herokuapp.com/'
+ authURL = 'https://backendmariano.herokuapp.com/auth'
 
   constructor(private httpClient: HttpClient) { }
 
   public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
-    return this.httpClient.post<any>(this.authURL + '/auth/nuevo', nuevoUsuario);
+    return this.httpClient.post<any>(this.authURL + '/nuevo', nuevoUsuario);
   }
 
   public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
-    return this.httpClient.post<JwtDto>(this.authURL + '/auth/login', loginUsuario);
+    return this.httpClient.post<JwtDto>(this.authURL + '/login', loginUsuario);
   }
 }
