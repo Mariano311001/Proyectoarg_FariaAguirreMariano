c29cda5f392e463fa6d77a9848189016736cc968
Mariano Aguirre <104844333+Mariano311001@users.noreply.github.com>
2022-09-01 21:17:03 -0300

    Cambios

--- BackEnd/src/main/java/com/portfolio/Mariano/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfolio/Mariano/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://frontendmariano.web.app")
 @RequestMapping("/personas")
 
 public class PersonaController {
