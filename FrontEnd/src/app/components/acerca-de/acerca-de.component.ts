6e8512c12a5303856577c5e3d6f1b04c1b66d0e7
Mariano Aguirre <104844333+Mariano311001@users.noreply.github.com>
2022-09-20 19:29:41 -0300

    Cambios

--- FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -6,12 +6,13 @@ import { PersonaService } from 'src/app/service/persona.service';
   templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css']
 })
+
 export class AcercaDeComponent implements OnInit {
  persona: persona =new persona("","","");
   constructor(public personaService: PersonaService) { }
 
   ngOnInit(): void {
     this.personaService.getPersona().subscribe(data => {this.persona = data})
-  }
+  };
 
 }
